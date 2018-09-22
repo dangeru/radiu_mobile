1ad1c9c4d097be6ca78eee34356fdb05c443ffad
Niles Rogoff <me@niles.xyz>
2018-09-22 17:47:12 -0400

    choose mp4

--- app/src/main/assets/radiu/static/js/app.js
+++ app/src/main/assets/radiu/static/js/app.js
@@ -1,12 +1,21 @@
 var audio = new Audio('static/etc/calicomp_startup.mp3');
+var filenames = ['tenor.mp4', 'tumblr_opnks5eWY81w2tequo1_500.mp4', 'va11halla.mp4', 'doze.mp4', 'doze8.mp4', 'e843f5e4507d83ef51df7fb27b4d0bb2.mp4', 'doze9.mp4', 'back.mp4', 'doze6.mp4', 'tumblr_or4na0hzyq1w7cvmoo1_400.mp4', 'doze5.mp4', 'aesthetic_anime.mp4', 'city.mp4', 'doze3.mp4', 'tumblr_oul9tv2iCp1vhvnzyo1_500.mp4', 'tumblr_osneazEg8P1wtuf8mo1_500.mp4', 'large.mp4', 'doze7.mp4', 'contact.mp4', 'doze2.mp4', 'doze4.mp4', 'original.mp4', '8ed7f59623c682eebd51f6c10b9d2dee.mp4', 'tumblr_nk8lr1LlAq1s0hk4xo1_500.mp4', '68b211810e941c519d868e8ee9359d7b.mp4'];
 $(document).ready(function () {
+  var video = document.getElementById("video");
+  var src = document.createElement("source");
+  src.setAttribute("type", "video/mp4");
+  src.setAttribute("src", "static/mp4/" + filenames[Math.floor(Math.random() * filenames.length)]);
+  video.appendChild(src);
+  //<source src="static/mp4/tumblr_nk8lr1LlAq1s0hk4xo1_500.mp4" type="video/mp4">
+  video.play();
   if(radiu.hasWelcomePlayed().toUpperCase != "TRUE") {
     audio.play();
     radiu.setWelcomePlayed(true);
   }
-  setInterval(radioTitle, 2000); // DEBUG
   // we're going to update our html elements / player every 15 seconds
   setInterval(radioTitle, 15000);
+  // and also once now
+  radioTitle();
   $('#vol_slider').on("change", function() {
       console.log($(this).val());
       //document.getElementById('track').volume = $(this).val() / 100;
